7a4e4435a519c1b4a1a9bfc9caab4ebe2998584f
Dione Ousseynou <dioneousseynou03@gmail.com>
2022-12-01 20:18:37 +0100

    V2.0

--- src/main/java/org/sid/ebankservice/web/BankAccountGraphQLController.java
+++ src/main/java/org/sid/ebankservice/web/BankAccountGraphQLController.java
@@ -4,9 +4,11 @@ import org.sid.ebankservice.entities.BankAccount;
 import org.sid.ebankservice.repository.BankAccountRepository;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
+@Controller
 public class BankAccountGraphQLController {
     //Injection du repository
     private final BankAccountRepository bankAccountRepository;
