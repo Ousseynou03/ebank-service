42ae4b02c1685236bd17a12aa1961f8a18cd56f1
Dione <dioneousseynou03@gmail.com>
2022-12-04 13:36:44 +0100

    V2.1

--- src/main/java/org/sid/ebankservice/repository/BankAccountRepository.java
+++ src/main/java/org/sid/ebankservice/repository/BankAccountRepository.java
@@ -2,12 +2,10 @@ package org.sid.ebankservice.repository;
 
 import org.sid.ebankservice.entities.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
+
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-=======
 
->>>>>>> c8517da (v1.0)
 public interface BankAccountRepository extends JpaRepository<BankAccount , String> {
 }
