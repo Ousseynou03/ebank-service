181835637ceb7bbc1e4bdd54692576abd771c8a8
Dione <dioneousseynou03@gmail.com>
2022-12-04 12:31:16 +0100

    V2.0

--- src/main/java/org/sid/ebankservice/web/BankAccountGraphQLController.java
+++ src/main/java/org/sid/ebankservice/web/BankAccountGraphQLController.java
@@ -3,10 +3,13 @@ package org.sid.ebankservice.web;
 import org.sid.ebankservice.entities.BankAccount;
 import org.sid.ebankservice.repository.BankAccountRepository;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
+@Controller
 public class BankAccountGraphQLController {
     //Injection du repository
     private final BankAccountRepository bankAccountRepository;
@@ -19,9 +22,17 @@ public class BankAccountGraphQLController {
     public List<BankAccount> accountsList(){
         return bankAccountRepository.findAll();
     }
+
+    //Récupération d'un compte sachant son identifiant
     @QueryMapping
     public BankAccount bankAccountById(@Argument String id){
         return bankAccountRepository.findById(id).
                 orElseThrow(()-> new RuntimeException(String.format("Account % not found", id)));
+    }
+
+    //Ajout d'un compte
+    @MutationMapping
+    public BankAccount addAccount(@Argument BankAccount bankAccount){
+        return bankAccountRepository.save(bankAccount);
     }
 }
